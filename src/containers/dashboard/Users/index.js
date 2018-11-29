0980fa7d598a68f69e2ec622249dddcb2c37e3cc
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-11-29 16:24:17 +0300

    Added deployment script

--- src/containers/dashboard/Users/index.js
+++ src/containers/dashboard/Users/index.js
@@ -84,8 +84,6 @@ class Users extends Component {
       </div>
     );
 
-    console.log(this.props);
-
     return (
       <>
         <div className={s.buttons}>
@@ -140,7 +138,7 @@ class Users extends Component {
                 <td>{format(user.registrationDate, 'DD MMM YY | HH:mm:ss') || ''}</td>
                 <td>{format(user.lastActivity, 'DD MMM YY | HH:mm:ss') || ''}</td>
                 <td>{user.sub}</td>
-                <td>{JSON.stringify(user.scope) || 'undefined'}</td>
+                <td className={s.wide}>{JSON.stringify(user.scope) || 'undefined'}</td>
                 <td>{renderControls(user)}</td>
               </tr>
             ))}
