31534403d4cd2a928310612fa1034e1c50ec41e4
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-11-29 16:23:27 +0300

    Updates

---
+++ precache-manifest.99f6f0600eda8182b877dd5bae705995.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "989d9c9ad5bf6e10446cd54ba5cbe092",
+    "url": "/static/media/icons-16.989d9c9a.ttf"
+  },
+  {
+    "revision": "5ce25f274c955f65201f",
+    "url": "/static/css/main.83684b70.chunk.css"
+  },
+  {
+    "revision": "6826f3559996aec061b807a4a2cb9624",
+    "url": "/static/media/icons-16.6826f355.eot"
+  },
+  {
+    "revision": "aa10731c5876c35770a6",
+    "url": "/static/js/1.aa10731c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f22a4ccb3983ce2ed7f42385b3c595a4",
+    "url": "/static/media/icons-16.f22a4ccb.woff"
+  },
+  {
+    "revision": "5ce25f274c955f65201f",
+    "url": "/static/js/main.5ce25f27.chunk.js"
+  },
+  {
+    "revision": "99a9521d3a99cc5a1b62e13a9ce229a7",
+    "url": "/static/media/icons-20.99a9521d.eot"
+  },
+  {
+    "revision": "96d076147091d133bccae03d9aeb8550",
+    "url": "/static/media/icons-20.96d07614.woff"
+  },
+  {
+    "revision": "b2eb84410450d6a35e6e02c4d1f6639e",
+    "url": "/static/media/icons-20.b2eb8441.ttf"
+  },
+  {
+    "revision": "aa10731c5876c35770a6",
+    "url": "/static/css/1.fc699865.chunk.css"
+  },
+  {
+    "revision": "3fcfa62ea8ca198514eb0033f445c652",
+    "url": "/index.html"
+  }
+];
